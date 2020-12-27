7dac539636d9b5e9dfabc2e4ccd58d94873e4180
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2020-12-28 01:00:47 +0700

    fix assets

--- app/assets/javascripts/random_rate.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var buttonElement = document.getElementById('leftclick');
-    buttonElement.addEventListener('click',function(event){
-        console.log('An event triggerd: ' + event.type);
-    });
-});
